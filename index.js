14be6c12941cbc1ccfac35cf6bdc57d613b8c635
Nazrul <heynazrul@gmail.com>
2023-06-07 18:25:46 +0600

    user role update api using patch

--- index.js
+++ index.js
@@ -25,16 +25,49 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db('bistroDB').collection('users');
     const menuCollection = client.db('bistroDB').collection('menu');
     const reviewCollection = client.db('bistroDB').collection('reviews');
     const cartCollection = client.db('bistroDB').collection('carts');
 
+    // users related API
+
+    app.get('/users', async(req, res) => {
+      const result = await  usersCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already existing in database' });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch('users/admin/:id', async(req,res) => {
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result)
+
+    })
+    // Menu related API
     // get all items in database collection "menu"     <<========
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
 
+    // Review related API
     // get all reviews from database collection "reviews"   <<=======
     app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray();
